4872b5939232a6c8c114f973f35aeb0e7b2b8518
kenrazalan <kennethrazalan29@gmail.com>
2021-02-12 17:23:40 +0800

    unlike like

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,58 @@ const Home = () =>{
             }
         }).then((res)=>res.json())
         .then(result=>{
-            console.log(result.posts);
+            console.log(result);
             setData(result.posts)
         })
     },[])
+
+    const likePost = (id)=>{
+        fetch('http://localhost:5000/like',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization": "Bearer "+localStorage.getItem("jwt")},
+            body:JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData = data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
+    
+    const unlikePost = (id)=>{
+        fetch('http://localhost:5000/unlike',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")},
+            body:JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData = data.map(item=>{
+                if(item._id==result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
     return(
         <div className="home">
             {data.map(item=>{
@@ -24,7 +72,10 @@ const Home = () =>{
                           <img src={item.photo}/>
                      </div>
                     <div className="card-content">
-                        <i class="material-icons">favorite</i>
+                        <i className="material-icons" >favorite</i>
+                        <i className="material-icons" onClick={()=>likePost(item._id)}>thumb_up</i>
+                        <i className="material-icons" onClick={()=>unlikePost(item._id)}>thumb_down</i>
+                            <h6>{item.likes.length} likes</h6>
                             <h6>{item.title}</h6>
                             <p>{item.body}</p>
                             <input type="text" placeholder="add comment"/>
