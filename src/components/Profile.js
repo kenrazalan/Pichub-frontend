ea8e2210c3598bc8c6aea5321cf0460b3f14d6bd
kenrazalan <kennethrazalan29@gmail.com>
2021-02-12 16:09:06 +0800

    profile data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React,{useContext, useEffect, useState} from 'react'
+import {UserContext} from '../App'
 
 const Profile = () =>{
+    const [pic,setPic] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    useEffect(()=>{
+        fetch('http://localhost:5000/myposts',{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then((result)=>{
+            console.log(result);
+            setPic(result.myposts)
+        })
+    },[])
+
     return(
         <div style={{
             maxWidth:"550px",
@@ -20,7 +35,7 @@ const Profile = () =>{
                 }} src={"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}/>
                 </div>
             <div>
-                <h4>Knnth asdas</h4>
+                <h4>{state?state.name:"loading"}</h4>
                 <div style={{
                     display: 'flex',
                     justifyContent:"space-between",
@@ -33,10 +48,13 @@ const Profile = () =>{
             </div>
             </div>
             <div className="gallery">
-                <img className="item" src={"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}/>
-                <img className="item" src={"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}/>
-                <img className="item" src={"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}/>
-                <img className="item" src={"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}/>
+                {pic.map(item=>{
+                    return(
+                        <img className="item" src={item.photo}/>
+                    )
+                })}
+               
+               
             </div>
         </div>
     )
