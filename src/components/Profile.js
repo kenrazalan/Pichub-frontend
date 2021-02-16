38189b10919ed33e5b55cd3b3f1e60373998394f
kenrazalan <kennethrazalan29@gmail.com>
2021-02-16 13:17:20 +0800

    updating profile picture

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,9 @@ import {UserContext} from '../App'
 const Profile = () =>{
     const [pic,setPic] = useState([])
     const {state,dispatch} = useContext(UserContext)
+    const [image,setImage] = useState("")
+    const [url,setUrl] = useState(undefined)
+
     useEffect(()=>{
         fetch('http://localhost:5000/myposts',{
             headers:{
@@ -15,16 +18,49 @@ const Profile = () =>{
             setPic(result.myposts)
         })
     },[])
+
+    useEffect(()=>{
+        if(image){
+            const data = new FormData()
+            data.append("file",image)
+            data.append("upload_preset","instagram-clone")
+            data.append("cloud_name","dtwrzv0of")
+            fetch("https://api.cloudinary.com/v1_1/dtwrzv0of/image/upload",{
+                method:"post",
+                body: data
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data.url)
+        
+                setUrl(data.secure_url)
+                localStorage.setItem("user",JSON.stringify({...state,pic:data.secure_url} ))
+                dispatch({type:"UPDATEPIC",payload:data.secure_url})
+            }).catch(error=>{
+                console.log(error)
+            })
+
+        }
+       
+
+    },[image])
+
+    const updatePhoto= (file)=>{
+        setImage(file)
+    }
     return(
         <div style={{
             maxWidth:"550px",
             margin:"0 auto"
         }}>
             <div style={{
+              margin:"18px 0",
+              borderBottom: "1px solid gray"
+            }}>
+            <div style={{
                 display:'flex',
                 justifyContent:"space-around",
-                margin:"18px 0",
-                borderBottom: "1px solid gray"
+
             }}>
                 <div>
                     <img style={{
@@ -33,6 +69,8 @@ const Profile = () =>{
                     borderRadius:"80px"
                 }} src={state?state.pic:"loading"}
                 alt="profile"/>
+
+                 
                 </div>
             <div>
                 <h4>{state?state.name:"loading"}</h4>
@@ -48,6 +86,16 @@ const Profile = () =>{
                     <h5>{state? state.following.length: "0"} following</h5>
                 </div>
             </div>
+            </div>
+                <div className="file-field input-field">
+                    <div className="btn #64b5f6 blue darken-2">
+                    <span>Upload Picture</span>
+                    <input type="file" onChange={(e)=>{updatePhoto(e.target.files[0])}}/>
+                        </div>
+                <div className="file-path-wrapper">
+                    <input className="file-path validate" type="text"/>
+                </div>
+                </div>
             </div>
             <div className="gallery">
                 {pic.map(item=>{
