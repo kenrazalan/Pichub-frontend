b93ed7b9a23de6f9b80fde0df01fcf4bb21ca147
kenneth <kennethrazalan29@gmail.com>
2021-03-30 22:47:52 +0800

    loop skeleton

--- src/components/SideSuggestions/SideSuggestions.js
+++ src/components/SideSuggestions/SideSuggestions.js
@@ -1,159 +1,159 @@
-import React,{useContext, useState,useEffect} from 'react'
-import {Skeleton} from '@material-ui/lab'
+import React, { useContext, useState, useEffect } from "react";
+import { Skeleton } from "@material-ui/lab";
 import styled from "styled-components";
 import { UserContext } from "../../App";
+import { OptionsIcon } from "../assets/Icons";
 
 const Wrapper = styled.div`
-.containers{
+  .containers {
     display: flex;
     max-width: 320px;
-    
+
     flex-direction: column;
     height: 100vh;
     position: fixed;
     margin-left: 50px;
     margin-top: 20px;
-}
-.profile-container{
+  }
+  .profile-container {
     display: flex;
     flex-direction: row;
-
-    
-}
-.profile-container-list{
+  }
+  .profile-container-list {
     display: flex;
     flex-direction: row;
     margin-bottom: 8px;
-    
-}
+  }
 
-.profile{
+  .profile {
     width: 66px;
     height: 66px;
     border-radius: 50%;
     margin-right: 12px;
     border: 3px solid #d6249f;
-}
-.profile2{
+  }
+  .profile2 {
     width: 35px;
     height: 35px;
     border-radius: 50%;
     margin-right: 19px;
-}
-h6{
+  }
+  h6 {
     font-size: 14px;
     text-align: left;
-}
-.list{
+  }
+  .list {
     margin-right: 19px;
-}
-.suggestion{
+  }
+  .suggestion {
     display: flex;
     flex-direction: row;
-    justify-content:  stretch;
+    justify-content: stretch;
     margin: 20px 0 10px 0;
-}
-.btns{
-  margin: auto 0 auto auto;
-}
-.name{
+  }
+  .btns {
+    margin: auto 0 auto auto;
+  }
+  .name {
     font-size: 14px;
-    font-weight: 800
-}
-.username{
+    font-weight: 800;
+  }
+  .username {
     font-size: 14px;
-    margin-top: 0
-  
+    margin-top: 0;
+  }
+`;
+let list = [];
+for (var i = 0; i < 5; i++) {
+  list.push(
+    <div style={{ display: "flex" }}>
+      <Skeleton
+        style={{ marginBottom: "10px" }}
+        animation="wave"
+        variant="circle"
+        width={40}
+        height={40}
+      />
+      <Skeleton
+        style={{
+          alignSelf: "center",
+          marginBottom: "10px",
+          marginLeft: "10px",
+        }}
+        animation="wave"
+        height={30}
+        width="80%"
+      />
+    </div>
+  );
 }
-
-`
 
 function SideSuggestions() {
-    const {state,dispatch} = useContext(UserContext)
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true);
+  const { state, dispatch } = useContext(UserContext);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACKEND_URL}/allusers`, {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("jwt"),
-          },
-        })
-          .then((user) => user.json())
-          .then((result) => {
-            setLoading(false);
-            setUsers(result);
-            console.log(state);
-          });
-      }, [state]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/allusers`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((user) => user.json())
+      .then((result) => {
+        setLoading(false);
+        setUsers(result);
+        console.log(state);
+      });
+  }, [state]);
 
-    
-    return (
-        <Wrapper>
-        <div className="containers">
-            <div className="profile-container">
-         
-             
-               <img className="profile" src={state && state.pic} alt="asas"/>
-            
-                <div className="list">
-                      <div className="name"><b>{state && state.name}</b></div>
-                      <div className="username" style={{color:"#8e8e8e"}}>{state && state.username}</div>
-                </div>
-                <span className="btns">Logout</span>
+  return (
+    <Wrapper>
+      <div className="containers">
+        <div className="profile-container">
+          <img className="profile" src={state && state.pic} alt="asas" />
+
+          <div className="list">
+            <div className="name">
+              <b>{state && state.name}</b>
             </div>
-
-            <div className="suggestion">
-                <div style={{color: "#8e8e8e",fontWeight: 900}}>Suggestions For You</div>
-                <h6 className="btns"><b>See all</b></h6>
+            <div className="username" style={{ color: "#8e8e8e" }}>
+              {state && state.username}
             </div>
+          </div>
+          <span className="btns"><OptionsIcon/></span>
+        </div>
 
-            <div >
-                {loading? 
-                 <> 
-                    <div style={{display: "flex"}}>
-                    <Skeleton style={{marginBottom: "10px"}} animation="wave" variant="circle" width={40} height={40} />
-                    <Skeleton style={{alignSelf: "center",marginBottom: "10px",
-                               marginLeft: "10px"}} animation="wave" height={30} width="80%" />
-                    </div>
-                    <div style={{display: "flex"}}>
-                    <Skeleton style={{marginBottom: "10px"}} animation="wave" variant="circle" width={40} height={40} />
-                    <Skeleton style={{alignSelf: "center",marginBottom: "10px",
-                               marginLeft: "10px"}} animation="wave" height={30} width="80%" />
-                    </div>
-                    <div style={{display: "flex"}}>
-                    <Skeleton style={{marginBottom: "10px"}} animation="wave" variant="circle" width={40} height={40} />
-                    <Skeleton style={{alignSelf: "center",marginBottom: "10px",
-                               marginLeft: "10px"}} animation="wave" height={30} width="80%" />
-                    </div>
-                    <div style={{display: "flex"}}>
-                    <Skeleton style={{marginBottom: "10px"}} animation="wave" variant="circle" width={40} height={40} />
-                    <Skeleton style={{alignSelf: "center",marginBottom: "10px",
-                               marginLeft: "10px"}} animation="wave" height={30} width="80%" />
-                    </div>
-                    <div style={{display: "flex"}}>
-                    <Skeleton style={{marginBottom: "10px"}} animation="wave" variant="circle" width={40} height={40} />
-                    <Skeleton style={{alignSelf: "center",marginBottom: "10px",
-                               marginLeft: "10px"}} animation="wave" height={30} width="80%" />
-                    </div>
-                            
-                </>
-               : users.slice(0,5).map(user=>(
-                 <div className="profile-container-list">
-                <img className="profile2" src={user.pic} alt="asas"/>
+        <div className="suggestion">
+          <div style={{ color: "#8e8e8e", fontWeight: 900 }}>
+            Suggestions For You
+          </div>
+          <h6 className="btns">
+            <b>See all</b>
+          </h6>
+        </div>
+
+        <div>
+          {loading ? (
+            <>{list}</>
+          ) : (
+            users.slice(0, 5).map((user) => (
+              <div className="profile-container-list">
+                <img className="profile2" src={user.pic} alt="asas" />
                 <div className="list">
-                      <div className="name"><b>{user.name}</b></div>
-                      <div className="username">{user.username}</div>
-                     
+                  <div className="name">
+                    <b>{user.name}</b>
+                  </div>
+                  <div className="username">{user.username}</div>
                 </div>
                 <span className="btns">Follow</span>
-            </div> 
-                ))}
-         
-             </div>
+              </div>
+            ))
+          )}
         </div>
-        </Wrapper>
-    )
+      </div>
+    </Wrapper>
+  );
 }
 
-export default SideSuggestions
+export default SideSuggestions;
